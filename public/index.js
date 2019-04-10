bc3f96c26eafd661017c8572567a842b682982a8
ashishtayal89 <ashishtayal14@gmail.com>
2019-04-10 09:41:53 +0530

    Add async code

--- public/index.js
+++ public/index.js
@@ -64,29 +64,50 @@ console.log(childObj.hasOwnProperty("lastname"));
 
 // 3. Class
 
-class Parent{
+class Parent1{
 }
-class Child extends Parent{
+class Child1 extends Parent1{
 }
-var childObj = new Child();
+var childObj = new Child1();
 
 
 // Async Programming
 
 // 1. Using Callbacks
 
-function asyncFunc(){
+(function asyncFunc(){
     setTimeout(()=>{
+        console.log(0);
+        let second = 1;
         setTimeout(()=>{
+            console.log(second);
+            let third = 2;
             setTimeout(()=>{
-
+                console.log(third);
             },1000)
         },1000)
     },1000)
-}
+})()
 
 // 2. Promise
 
-var myPromise = new Promise((resolve)=>setTimeout(()=>resolve,1000))
-                    .then(()=>setTimeout(()=>{},1000))
-                    .then(()=>setTimeout(()=>{},1000));
+var myPromise = new Promise((resolve)=>setTimeout(()=>{console.log(0);resolve(1)},1000))
+                    .then((value)=>new Promise((resolve)=>setTimeout(()=>{console.log(value);resolve(2)},1000)))
+                    .then((value)=>setTimeout(()=>{console.log(value)},1000));
+
+// 3. Async/Await
+
+/*The AsyncFunction constructor creates a new async function object. In JavaScript every asynchronous function is actually an AsyncFunction object. Object.getPrototypeOf(async function(){}).constructor */
+
+async function asyncDemo(){
+    let result = await new Promise((resolve)=>setTimeout(()=>resolve(0),1000));
+    console.log(result);
+    result = await new Promise((resolve)=>setTimeout(()=>resolve(1),1000));
+    console.log(result);
+    result = await new Promise((resolve)=>setTimeout(()=>resolve(2),1000));
+    return result;
+}
+var asyncPromise = asyncDemo();
+asyncPromise.then((value)=>console.log(value));
+
+console.log(Object.getPrototypeOf(async function(){}).constructor)
